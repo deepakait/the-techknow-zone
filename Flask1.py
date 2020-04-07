07be1f30cc516c2a182f3efc10516a16a0333d9c
Deepak Kumar <aitdeepak11624@gmail.com>
2020-04-07 23:47:57 +0530

    initial commmit

---
+++ Flask1.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+
+    return render_template('contact.html')
+
+
+@app.route("/home")
+def index():
+
+    return render_template('index.html')
+
+
+@app.route("/index")
+def ind():
+
+    return render_template('index.html')
+
+
+@app.route("/post")
+def post():
+
+    return render_template('post.html')
+
+
+app.run(debug=True)
---
+++ README.md
@@ -0,0 +1,1 @@
+# the-techknow-zone
